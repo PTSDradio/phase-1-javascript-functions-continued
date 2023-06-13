f632aa1772e847b87a657b3fab380b5d9625f6c8
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-06-13 08:29:19 -0500

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(act = "roller-skate") {
+    return`This Saturday, I want to ${act}!`
+}
+
+function mondayWork(act= "go to the office") {
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(n="*") {
+return function (des="special") {
+    return `You are ${n}${des}${n}!`
+}
+} 
